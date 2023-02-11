507b2b25d4e96dd65ec7cde264bae51a9fac5a09
Yanters <yanter@wp.pl>
2023-02-11 18:51:42 +0100

    Exercises component

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,7 +1,35 @@
+import { Box, Stack, Typography } from '@mui/material';
 import React from 'react';
 
-const Exercises = () => {
-  return <div>Exercises</div>;
+const Exercises = ({ bodyPart, setExercises, exercises }) => {
+  console.log(exercises);
+
+  return (
+    <Box
+      id='exercises'
+      sx={{
+        mt: { lg: '110px' },
+      }}
+      mt='50px'
+      p='20px'
+    >
+      <Typography variant='h4' mb='46px'>
+        {bodyPart === 'All' ? 'All Exercises' : bodyPart}
+      </Typography>
+      <Stack
+        direction='row'
+        flexWrap='wrap'
+        justifyContent='center'
+        sx={{
+          gap: { lg: '110px', xs: '50px' },
+        }}
+      >
+        {exercises.map((exercise, index) => (
+          <p>{exercise.name}</p>
+        ))}
+      </Stack>
+    </Box>
+  );
 };
 
 export default Exercises;
