dd8d26f9c4e344c97d629f7d28e92f9a40a53f3c
Yanters <yanter@wp.pl>
2023-02-11 13:30:36 +0100

    Search Exercises

--- src/components/SearchExcercises.jsx
+++ src/components/SearchExcercises.jsx
@@ -1,7 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 const SearchExcercises = () => {
-  return <div>SearchExcercises</div>;
+  const [search, setSearch] = useState('');
+
+  const handleSearch = async () => {
+    if (search && search.length > 0) {
+      // const exercisesData = ...
+    }
+  };
+
+  return (
+    <Stack alignItems='center' mt='40px' justifyContent='center' p='20px'>
+      <Typography
+        fontWeight='700'
+        sx={{
+          fontSize: { lg: '44px', xs: '30px' },
+        }}
+        mb='20px'
+        textAlign='center'
+      >
+        Find yourself a new workout
+      </Typography>
+      <Box position='relative' mb='72px'>
+        <TextField
+          type='text'
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          placeholder='Search for an exercise'
+          sx={{
+            width: { lg: '800px', xs: '350px' },
+            height: '50px',
+            input: {
+              fontWeight: '700',
+              border: 'none',
+              borderRadius: '10px',
+              backgroundColor: '#FFF',
+            },
+          }}
+        />
+        <Button
+          className='search-btn'
+          onClick={handleSearch}
+          sx={{
+            backgroundColor: '#FF2625',
+            color: '#FFF',
+            textTransform: 'none',
+            width: { lg: '200px', xs: '100px' },
+            fontSize: { lg: '20px', xs: '14px' },
+            height: '56px',
+            position: 'absolute',
+            right: '0',
+          }}
+        >
+          <svg
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              d='M15.5 14H14.71L14.43 13.73C15.41 12.59 16 11.11 16 9.5C16 5.91 13.09 3 9.5 3C5.91 3 3 5.91 3 9.5C3 13.09 5.91 16 9.5 16C11.11 16 12.59 15.41 13.73 14.43L14 14.71V15.5L19 20.49L20.49 19L15.5 14ZM9.5 14C7.01 14 5 11.99 5 9.5C5 7.01 7.01 5 9.5 5C11.99 5 14 7.01 14 9.5C14 11.99 11.99 14 9.5 14Z'
+              fill='currentColor'
+            />
+          </svg>
+          &nbsp;Search
+        </Button>
+      </Box>
+    </Stack>
+  );
 };
 
 export default SearchExcercises;
